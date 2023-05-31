d1b9dfe8548e1cc1c6b4a65983c1f9d09f8e2917
5TFG4 <18154772+5TFG4@users.noreply.github.com>
2023-05-30 23:58:28 -0400

    Add basic add async job method

--- src/GLaDOS/glados.py
+++ src/GLaDOS/glados.py
@@ -1,6 +1,33 @@
 import asyncio
+from collections.abc import (
+    Awaitable,
+    Callable,
+    Collection,
+    Coroutine,
+    Iterable,
+    Mapping,
+)
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    NamedTuple,
+    ParamSpec,
+    TypeVar,
+    cast,
+    overload,
+)
 
 from GLaDOS import CoreState
+
+_R = TypeVar("_R")
+_CallableT = TypeVar("_CallableT", bound=Callable[..., Any])
+
+def callback(func: _CallableT) -> _CallableT:
+    """Annotation to mark method as safe to call from within the event loop."""
+    setattr(func, "_hass_callback", True)
+    return func
+
 
 class GLaDOS:
     _instance = None
@@ -25,6 +52,39 @@ class GLaDOS:
             #trade = Trade.from_dict(trade_data[0])
             self.event_handler.handle_trade(trade_data[0])
 
+
+    @callback
+    def async_add_hass_job(
+        self, hassjob: HassJob[..., Coroutine[Any, Any, _R] | _R], *args: Any
+    ) -> asyncio.Future[_R] | None:
+        """Add a HassJob from within the event loop.
+
+        This method must be run in the event loop.
+        hassjob: HassJob to call.
+        args: parameters for method to call.
+        """
+        task: asyncio.Future[_R]
+        if hassjob.job_type == HassJobType.Coroutinefunction:
+            if TYPE_CHECKING:
+                hassjob.target = cast(
+                    Callable[..., Coroutine[Any, Any, _R]], hassjob.target
+                )
+            task = self.loop.create_task(hassjob.target(*args), name=hassjob.name)
+        elif hassjob.job_type == HassJobType.Callback:
+            if TYPE_CHECKING:
+                hassjob.target = cast(Callable[..., _R], hassjob.target)
+            self.loop.call_soon(hassjob.target, *args)
+            return None
+        else:
+            if TYPE_CHECKING:
+                hassjob.target = cast(Callable[..., _R], hassjob.target)
+            task = self.loop.run_in_executor(None, hassjob.target, *args)
+
+        self._tasks.add(task)
+        task.add_done_callback(self._tasks.remove)
+
+        return task
+    
 
     async def async_run(self, *, attach_signals: bool = True) -> int:
         """Weaver main entry point.
@@ -51,5 +111,8 @@ class GLaDOS:
         return self.exit_code
     
     async def async_start(self) -> None:
+        self.state = CoreState.starting
+
         print("Glados async_start")
-        return
+
+        self.state = CoreState.running
