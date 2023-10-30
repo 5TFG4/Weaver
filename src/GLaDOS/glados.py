1a250b7a17aff245c12057439fb5ed8619c9629b
TFG <18154772+5TFG4@users.noreply.github.com>
2023-10-30 12:22:51 -0400

    Test Async

--- src/GLaDOS/glados.py
+++ src/GLaDOS/glados.py
@@ -3,13 +3,20 @@ import asyncio
 class GLaDOS:
     async def handle_event(self, event):
         # 处理事件逻辑
-        print('test')
+        print('test1')
         pass
 
-    async def get_event(self):
-        return await asyncio.sleep(1)  # 返回一个延时事件
+    async def handle_event2(self, event):
+        # 处理事件逻辑
+        print('test2')
+        pass
+
+    async def get_event(self, time):
+        return await asyncio.sleep(time)  # 返回一个延时事件
 
     async def start(self):
         while True:
-            event = await self.get_event()
+            event = await self.get_event(1)
+            event2 = await self.get_event(2)
             await self.handle_event(event) 
+            await self.handle_event2(event2) 
