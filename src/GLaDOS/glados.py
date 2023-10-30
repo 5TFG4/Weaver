beedeaaa581ac2c2a447a7e3ac11c135f88a6b42
TFG <18154772+5TFG4@users.noreply.github.com>
2023-10-30 15:15:09 -0400

    Imporve Async Framework

--- src/GLaDOS/glados.py
+++ src/GLaDOS/glados.py
@@ -1,6 +1,10 @@
 import asyncio
+from random import random
 
 class GLaDOS:
+    def __init__(self):
+        self.stop_event = asyncio.Event()
+
     async def handle_event(self, event):
         # 处理事件逻辑
         print('test1')
@@ -9,14 +13,25 @@ class GLaDOS:
     async def handle_event2(self, event):
         # 处理事件逻辑
         print('test2')
+        if random() < 0.2:
+          self.stop_event.set()
         pass
 
     async def get_event(self, time):
         return await asyncio.sleep(time)  # 返回一个延时事件
+    
+    async def print_test1_loop(self):
+        while not self.stop_event.is_set():
+            event1 = await self.get_event(1)
+            await self.handle_event(event1)
+    
+    async def print_test2_loop(self):
+        while not self.stop_event.is_set():
+            event2 = await self.get_event(2)
+            await self.handle_event2(event2)
 
     async def start(self):
-        while True:
-            event = await self.get_event(1)
-            event2 = await self.get_event(2)
-            await self.handle_event(event) 
-            await self.handle_event2(event2) 
+        await asyncio.gather(
+            self.print_test1_loop(),
+            self.print_test2_loop()  
+        )
